846866e61f83929851aa43904bbf21a1d9336dd9
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-03 14:55:00 -0700

    ‘hi’

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == ord(q) or i == ord(e):
-        continue
-    print("{:c}".format(i), end = '')
+    if i != ord('q') and i != ord('e'):
+        print("{:c}".format(i), end = '')
