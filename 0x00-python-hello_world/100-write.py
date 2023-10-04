9aa8faf26769022d6bfa6e4d432f05b294222755
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-04 13:33:38 -0700

    ‘hi’

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
-str = 'and that piece of art is useful - Dora Korpar, 2015-10-19'
-for i in range(len(str)):
-    sys.stdout.write(str[i])
+
+str_ = 'and that piece of art is useful - Dora Korpar, 2015-10-19'
+for i in range(len(str_)):
+    sys.stdout.write(str_[i])
 sys.stdout.write('\n')
 sys.exit(1)
