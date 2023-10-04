ed64751d289943d7e9dc22c251f6a4e7a3c11a04
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-04 13:30:24 -0700

    ‘hi’

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+
+str = 'and that piece of art is useful - Dora Korpar, 2015-10-19'
+for i in range(len(str)):
+    sys.stdout.write(str[i])
+sys.stdout.write('\n')
+sys.exit(1)
