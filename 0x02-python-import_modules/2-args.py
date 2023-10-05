50b2055aa07b0e36485262d2d416321e7953aa46
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-05 16:22:25 -0700

    ‘hi’

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,10 @@ if __name__ == "__main__":
 
     if len(argv) == 1:
         print("0 arguments.")
-
     elif len(argv) == 2:
         print("1 argument:")
-        print("1:", argv[1])
-
+        print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(len(argv)-1))
-        for i, arg in enumerate(argv[1:], 1):
-            print("{}:{}".format(i, arg))
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
