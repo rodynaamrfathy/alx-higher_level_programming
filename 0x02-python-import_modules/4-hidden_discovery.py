17924b5e1e8e8ac97a9875c4e6d0aa7704c267dc
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-05 16:48:07 -0700

    ‘hi’

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-
-    for name in dir(hidden_4):
+    for name in sorted(dir(hidden_4)):
         if name[0] == name[1] == '_':
             continue
         else:
