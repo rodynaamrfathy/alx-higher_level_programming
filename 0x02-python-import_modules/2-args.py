e5b0df6b78c8929f66ab6efe5a747d9025e7a6e5
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-05 16:19:42 -0700

    ‘hi’

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 1:
+        print("0 arguments.")
+
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1:", argv[1])
+
+    else:
+        print("{} arguments:".format(len(argv)-1))
+        for i, arg in enumerate(argv[1:], 1):
+            print("{}:{}".format(i, arg))
