4193d46da1633d35c7851abd4fc7beecb4a9c76d
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-09 10:06:17 -0700

    ‘hi’

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    transposed = [[row[i] for row in matrix] for i in range(len(matrix))]
+    
+    for row in transposed:
+        for item in row:
+            print("{:d}".format(item))
