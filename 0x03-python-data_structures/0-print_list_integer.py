862c95a7519f7ce1addd906f71967b3adeee5759
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-09 09:04:45 -0700

    ‘hi’

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if my_list is None:
-        my_list = []  # Create a new empty list if my_list is not provided
     for item in my_list:
-            print("{}".format(item))
+        print("{:d}".format(item))
