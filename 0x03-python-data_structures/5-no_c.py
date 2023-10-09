d4173f9bc8edbb01c584b3bb9875aec5e4a485cb
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-09 09:51:43 -0700

    ‘hi’

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-            if my_string[i] == 'c' or my_string[i] == 'C':
-                my_string.remove(my_string[i])
-    return my_string
+    result = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result += char
+    return result
