e46a51f06560f4c58d3c5b1d7f9deec68d453f67
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-09 10:14:09 -0700

    ‘hi’

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    transposed = [[row[i] for row in matrix] for i in range(len(matrix))]
-    
-    for row in transposed:
-        for item in row:
+    if not matrix:
+        return None
+    for row in matrix:
+        for item in matrix:
             print("{:d}".format(item), end=' ')
         print()
