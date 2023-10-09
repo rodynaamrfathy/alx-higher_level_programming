fa7b1f4038dba7df5b986413720bb660f7d3853b
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-09 10:32:24 -0700

    ‘hi’

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return None
     for row in matrix:
         for item in row:
-            print("{:d}".format(item), end=' ')
+            print("{:d}".format(item), end=" " if item != row[-1] else "")
         print()
