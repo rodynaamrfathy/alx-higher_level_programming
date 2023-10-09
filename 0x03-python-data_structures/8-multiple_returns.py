9310ad1a1db7077bcf5ecfec88c96c012b5310fb
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-09 10:59:06 -0700

    ‘hi’

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
-        length =  len(sentence)
-        first_char = = sentence[0]
-        t = tuple((length, first_char))
-        return t
-    else:
-        return 0, None
+    length =  len(sentence)
+    first_char = = sentence[0] if sentence else None
+    t = (length, first_char)
+    return t
