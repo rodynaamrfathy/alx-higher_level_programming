a0d578b3d1fd287d1fb46fc1cf26298108ae7233
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-09 10:41:34 -0700

    ‘hi’

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    add1 = (tuple_b[0] if not tuple_b[0] else 0) + \
-    (tuple_a[0] if not tuple_a[0] else 0)
-    add2 = (tuple_b[1] if not tuple_b[1] else 0) + \
-    (tuple_a[1] if not tuple_a[1] else 0)
+    add1 = (tuple_b[0] if 2 < tuple_b[0] else 0) + \
+    (tuple_a[0] if 2 < tuple_a[0] else 0)
+    add2 = (tuple_b[1] if 2 < tuple_b[1] else 0) + \
+    (tuple_a[1] if 2 < tuple_a[1] else 0)
     add_tuple = (add1, add2)
