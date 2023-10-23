d479c7c7d87ea3c0bd269fe607051b4a0e93d16c
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-23 13:09:52 -0700

    ‘hi’

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        counter = 0
+        for i in range(x):
+            print("{}".format(my_list[i]), endl='')
+            counter +=
+    except IndexError:
+        None
+    print()
+    return count
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,2 @@
+ this is my repo for high level programming
+ this is my dir for Exceptions Why Python programming is awesome
