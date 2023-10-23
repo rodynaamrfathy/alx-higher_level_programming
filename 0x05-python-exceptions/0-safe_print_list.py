23772aaa7b8acb7cad0abfad5e1534bc144e4232
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-23 13:12:18 -0700

    ‘hi’

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
         counter = 0
         for i in range(x):
             print("{}".format(my_list[i]), endl='')
-            counter +=
+            counter += 1
     except IndexError:
         None
     print()
