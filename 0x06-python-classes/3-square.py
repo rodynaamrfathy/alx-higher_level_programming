f062d22f770c20c6360c4846e8db5987d6a842c6
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-27 12:01:52 -0700

    ‘hi’

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square. that sets a private size for the square"""
+    def __init__(self, size=0):
+
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+
+        """ returns the current square area. """
+        return (self.__size ** 2)
