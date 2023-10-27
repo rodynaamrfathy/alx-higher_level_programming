d101019195a08b12a7d2a0b45e3df690af4f3a7f
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-27 11:40:18 -0700

    ‘hi’

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,4 +4,11 @@
 class Square:
     """Represents a square. that sets a private size for the square"""
     def __init__(self, size):
+
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+
         self.__size = size
