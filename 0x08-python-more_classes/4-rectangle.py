18fa0526d97791b3077dcdbdbfe7dad29f75b959
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-31 17:33:31 -0700

    ‘hi’

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -67,6 +67,5 @@ class Rectangle:
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width) + ", "  + str(self.__height)
-        rect += ")"
+        rect = "Rectangle({}, {})".format(str(self.__width), str(self.__height))
         return rect
