87c6ff7d12b892041abb69799e4c12856fa9b8a7
rodynaamrfathy <rodynaamr@icloud.com>
2023-10-31 18:00:10 -0700

    ‘hi’

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,7 +8,6 @@ class Rectangle:
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
     """
-    
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
