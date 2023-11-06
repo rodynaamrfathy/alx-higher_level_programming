7373a019671c620ee0f8ccac1c8c5f3b217270ea
rodynaamrfathy <rodynaamr@icloud.com>
2023-11-06 14:50:18 -0800

    ‘hi’

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class with public instance methods"""
+
+    def area(self):
+        """ raises an Exception with the message area() """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+
+        Args:
+            name (string): name
+            value (int): value
+        """
+
+        self.name = name
+        if !isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
