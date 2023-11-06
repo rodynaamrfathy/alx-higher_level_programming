76840074cd9dbb30ca4f3091ab59f5a98c76c71a
rodynaamrfathy <rodynaamr@icloud.com>
2023-11-06 14:43:11 -0800

    ‘hi’

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ raises an Exception with the message area() """
+
+    def area(self):
+        """ raises an Exception with the message area() """
+        raise Exception("area() is not implemented")
