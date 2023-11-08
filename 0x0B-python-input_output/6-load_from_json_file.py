d938a2a99ebbdc14f449b004b4a7ba23032ba3ff
rodynaamrfathy <rodynaamr@icloud.com>
2023-11-08 12:22:10 -0800

    ‘hi’

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function that creates an Object from a “JSON file”"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """a function that creates an Object from a “JSON file”"""
+
+    json.load(filename)
