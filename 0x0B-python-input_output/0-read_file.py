ae0d4d380ef8fc2b5d3895c9be830b7928e68294
rodynaamrfathy <rodynaamr@icloud.com>
2023-11-08 10:46:03 -0800

    ‘hi’

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """ a function that reads a text file (UTF8)"""
 
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end='')
