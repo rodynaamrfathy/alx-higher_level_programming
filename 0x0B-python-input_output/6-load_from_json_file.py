7cc65588c0ff85a0b1f771adbb7d58fa09a36d7a
rodynaamrfathy <rodynaamr@icloud.com>
2023-11-08 12:24:19 -0800

    ‘hi’

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 def load_from_json_file(filename):
     """a function that creates an Object from a “JSON file”"""
 
-    with open(filename, "r")
+    with open(filename, "r") as f:
         json.load(f)
