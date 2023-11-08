516fd266938faec9d286c12ac543b863d34a3c95
rodynaamrfathy <rodynaamr@icloud.com>
2023-11-08 11:55:34 -0800

    ‘hi’

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defining to_json_string function"""
+
+def to_json_string(my_obj):
+    """ a function that returns the JSON representation of an object (string)"""
+
+    return json.dumps(my_obj)
