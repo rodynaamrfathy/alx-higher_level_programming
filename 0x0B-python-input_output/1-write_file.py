59013deed8baf176dde8e02d121d66458257e360
rodynaamrfathy <rodynaamr@icloud.com>
2023-11-08 10:59:12 -0800

    ‘hi’

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-"""a function that writes a string to a text file (UTF8) and returns the number of characters written"""
+"""a function that writes a string to a text file (UTF8) 
+and returns the number of characters written"""
 
 
 def write_file(filename="", text=""):
+    """function that writes a string to a text file"""
 
+    with open(filename, "w", encoding="utf-8") as f:
+        content = f.write(text)
+        return(content)
