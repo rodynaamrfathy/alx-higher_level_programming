59ecab89d96ec06c0e962f9e7acf690b2ccd5a84
rodynaamrfathy <rodynaamr@icloud.com>
2023-12-12 14:46:25 -0800

    ‘SQL’

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
 --  a script that lists all rows of the table first_table 
 -- from the database hbtn_0c_0 in your MySQL server.
 -- The database name will be passed as an argument of the mysql command
-SELECT COLUMN * FROM `first_table`;
+SELECT * COLUMN FROM `first_table`;
