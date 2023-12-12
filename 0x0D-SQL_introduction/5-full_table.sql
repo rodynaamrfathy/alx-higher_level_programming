8096311d0c959840fc374ac77c50b620f4642b38
rodynaamrfathy <rodynaamr@icloud.com>
2023-12-12 14:39:43 -0800

    ‘SQL’

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 --  a script that prints the full description of the table first_table
 -- from the database hbtn_0c_0 in your MySQL server.
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+SHOW TABLE `first_table` FROM hbtn_0c_0;
