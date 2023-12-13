c732b52d0a6a3242223db0b317e13f49fb7401f5
rodynaamrfathy <rodynaamr@icloud.com>
2023-12-13 12:58:40 -0800

    ‘SQL’

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- a script that creates the table id_not_null on  MySQL server.
+-- id INT with the default value 1
+CREATE TABLE IF NOT EXISTS id_not_null(
+	id INT NOT NULL DEFAULT 1,
+	name VARCHAR(256)
+);
