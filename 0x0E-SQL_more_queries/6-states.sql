2c707ea6792103b4d8dc9c9a5621d14e2ee38c16
rodynaamrfathy <rodynaamr@icloud.com>
2023-12-13 13:10:06 -0800

    ‘SQL’

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,9 +1,9 @@
 -- a script that creates the database hbtn_0d_usa
 -- and the table states (in the database hbtn_0d_usa)
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- CREATE TABLE
 USE hbtn_0d_usa
-CREATE TABLE states(
+CREATE TABLE IF NOT EXISTS states(
 	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
