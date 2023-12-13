e2f92e088d3130f27c72938acfa74609297dccfc
rodynaamrfathy <rodynaamr@icloud.com>
2023-12-13 13:01:43 -0800

    ‘SQL’

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+--  a script that creates the table unique_id on MySQL server.
+-- id INT with the default value 1 and must be unique
+CREATE TABLE IF NOT EXISTS unique_id(
+	id INT NOT NULL DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+);
