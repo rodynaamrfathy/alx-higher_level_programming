937a6e69a268921e15ea5bafde1cc76631ba8be7
rodynaamrfathy <rodynaamr@icloud.com>
2023-12-13 12:55:23 -0800

    ‘SQL’

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
--- a script that creates the table force_name on your MySQL server.
+-- a script that creates the table force_name on MySQL server.
+-- creates a tabl
 CREATE TABLE IF NOT EXISTS force_name(
 	id INT,
 	namr VARCHAR(256) NOT NULL
