9253092e67bfb39aff3b84522e84a611bed60de2
rodynaamrfathy <rodynaamr@icloud.com>
2024-02-07 13:04:40 -0800

    ‘SQL’

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 -- lists all rows of a database meeting a condition
-SELECT tv_genres.name AS 'name'
+SELECT tv_genres.name AS 'name',
 COUNT (tv_show_genres.genre_id AS 'no')
 FROM tv_genres
 RIGHT JOIN tv_show_genres
