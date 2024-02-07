5b35c5a47de2e84bf5719649ddf11eb487d53961
rodynaamrfathy <rodynaamr@icloud.com>
2024-02-07 13:18:18 -0800

    ‘SQL’

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- uses a databse to lists all rows in a table corresponding to all rows in another
+SELECT tv_genres.name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows on tv_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
