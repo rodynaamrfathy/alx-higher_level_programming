127e69326e238ef1dc7a478c1ab227620d7ca458
rodynaamrfathy <rodynaamr@icloud.com>
2024-02-07 12:45:56 -0800

    ‘SQL’

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,4 +4,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.title = tv_show_genres.genre_id
-ORDER BY tv_shows.title,tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
