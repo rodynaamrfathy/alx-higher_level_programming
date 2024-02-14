49437b528636f830ffa3082bc478b3245a2d9cfb
rodynaamrfathy <rodynaamr@icloud.com>
2024-02-13 16:19:16 -0800

    ‘JS’

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,32 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print(){
+    let s = '';
+    for (let i = 0; i < this.width; i++){
+      s += 'X'
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(s);
+    }
+  }
+  
+  rotate(){
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double(){
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
