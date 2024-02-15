4b18b87f0e6e9a3425db91f3f6f2b9de2aa3a78c
rodynaamrfathy <rodynaamr@icloud.com>
2024-02-15 06:54:01 -0800

    ‘JS’

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,36 +1,8 @@
 #!/usr/bin/node
-
-if (process.argv[2] === undefined || isNaN(process.argv[2]) || Number(process.argv[2]) === 1) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  let max = 0;
-
-  // Find the max value in the arguments
-  for (let i = 2; i < process.argv.length; i++) {
-    if (Number(process.argv[i]) > max) {
-      max = Number(process.argv[i]);
-    }
-  }
-
-  const newlist = [];
-  let j = 0;
-
-  // Fill newlist without the max value
-  for (let i = 2; i < process.argv.length; i++) {
-    if (Number(process.argv[i]) !== max) {
-      newlist[j] = Number(process.argv[i]);
-      j++;
-    }
-  }
-
-  max = 0;
-
-  // Find the second largest value in newlist
-  for (let i = 0; i < newlist.length; i++) {
-    if (max < newlist[i]) {
-      max = newlist[i];
-    }
-  }
-
-  console.log(max);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
