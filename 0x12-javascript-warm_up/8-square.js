64d7052ac1d3398a68314c577dca9030f7f1263f
rodynaamrfathy <rodynaamr@icloud.com>
2024-02-15 06:44:19 -0800

    ‘JS’

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (process.argv[2] == undefined || isNaN(process.argv[2])){
+	console.log("Missing size");
+} else{
+  const x = Number(process.argv[2]);
+  let s = '';
+  for (let i = 0; i < x; i++){
+    s += 'X';
+  }
+  for (let i = 0; i < x; i++){
+    console.log(s);
+  }
+}
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function add(a, b){
+  if (a === undefined || b === undefined || isNaN(a) || isNaN(b)){	
+    return 'NaN';
+  }
+  return Number(a) + Number(b);
+}
+console.log(add(process.argv[2], process.argv[3]));
