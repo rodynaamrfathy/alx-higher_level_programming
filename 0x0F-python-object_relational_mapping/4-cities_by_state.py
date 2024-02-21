f535b2821d0bd2ba0985e6da6db8841ba831bdbb
rodynaamrfathy <rodynaamr@icloud.com>
2024-02-21 11:09:14 -0800

    ‘MySQLdb’

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute("""
-                SELECT cities.id, cities.name, states.id  FROM cities
+                SELECT cities.id, cities.name, states.name  FROM cities
                 INNER JOIN states ON states.id=cities.state_id
                 """)
     rows = cur.fetchall()
